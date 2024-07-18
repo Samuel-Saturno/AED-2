aa10c8ce8e112b2416304f60f1f99683ae5164fd
Samuel-Saturno <111779961+Samuel-Saturno@users.noreply.github.com>
2024-07-18 17:11:18 -0300

    Update questão.c

--- Unidade 1/Atividade-2/questão.c
+++ Unidade 1/Atividade-2/questão.c
@@ -1,31 +1,28 @@
 /*
 OBS: n são as chamadas recursivas
-+ Chamada Recursiva: 2ns => n * 2 =
-+ Retorno de chamada recursiva: 1ns => n * 1 =
-+ Atribuição e Soma: 0,5 =>
-+ Subtração: 0,3 =>
++ Chamada Recursiva: 2ns => 979 * 2 =
++ Retorno de chamada recursiva: 1ns => 326 * 1 = 
++ Atribuição e Soma: 0,5 => 0ns e 326
++ Subtração: 0,3 => 
 */
 
 #include <stdio.h>
 
-//int count = 0;
+int count = 0;
 
 int fib3(int n) {
-    //count++;
-    //int x1, x2;
+    
     if(n < 3){
+        //count++;
         return 1;
     } else{
-        //x1 = fib3(n - 1);
-        //x2 = fib3(n - 2);
+        //count++;
         return fib3(n - 1) + fib3(n-2) + fib3(n-3);
     }
 }
 
-/*
 int main(){
     fib3(12);
     printf("%d", count);
     return 0;
 }
-*/
